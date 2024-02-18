6454383ee76cc570c29fab0debd3abc1cfbe8e11
Harry Collins <sensorydog@gmail.com>
2024-02-18 15:11:46 +1000

    new stuff!

--- DSFWORKS/DsfwReader.cs
+++ DSFWORKS/DsfwReader.cs
@@ -23,10 +23,41 @@ namespace DSFWORKS
                     ph = ph.Replace("_write ", "");
                     File.WriteAllText(FileLoc, ph);
                 }
-                else if (line.Contains("_read "))
+                else if (line.Contains("_read"))
                 {
-                    string ph = File.ReadAllText(FileLoc);
+                    if (File.Exists(FileLoc))
+                    {
+                        string ph = File.ReadAllText(FileLoc);
+                        Console.WriteLine(ph);
+                    }
+                    else
+                    {
+                        Console.WriteLine("failed to read: file doesn't exist? written file location: " + FileLoc);
+                    }
+                }
+                else if (line.Contains("_location "))
+                {
+                    string ph = line.Trim();
+                    ph = ph.Replace("_location ", "");
+                    FileLoc = ph;
+                }
+                else if (line.Contains("_log "))
+                {
+                    string ph = line;
+                    ph = ph.Replace("_log ", "");
                     Console.WriteLine(ph);
+                }
+                else if (line.Contains("_clear"))
+                {
+                    Console.Clear();
+                }
+                else if (line.Contains("//"))
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("no command or no proper command: " + line);
                 }
             }
         }
